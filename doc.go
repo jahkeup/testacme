5176db8bb3fa39051eeabad5111030eb00702423
Jacob Vallejo <jake@jahkeup.com>
2023-02-13 16:14:13 -0800

    feat: add a license
    Signed-off-by: Jacob Vallejo <jake@jahkeup.com>

---
+++ doc.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+// Package testacme provides a testing focused helper library which enables test
+// authors to execute ACME integration and functional tests. The ACME server
+// exercises challenge verifications with support for local-only environments by
+// way of an included DNS resolver and convenience wrappers for Pebble.
+//
+// https://github.com/letsencrypt/pebble
+//
+// See package's test source files for example usages.
+package testacme
