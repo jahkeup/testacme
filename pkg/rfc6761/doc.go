8d3d279a5c5f42534f0b6fb7f201319a7b4a1e83
Jacob Vallejo <jake@jahkeup.com>
2023-02-20 12:20:08 -0800

    fix: add license header

--- pkg/rfc6761/doc.go
+++ pkg/rfc6761/doc.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 // Package rfc6761 provides helpers for a subset of rfc6761 domain TLD.
 // Specifically, helpers are offered to build and work with .test TLD names for
 // testing purposes.
